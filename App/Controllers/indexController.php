989a0d92e91b4547958844df86fab289d5ec35f6
João Oneta <joaomatheusoneta01@gmail.com>
2022-03-21 16:11:09 -0300

    Implementada a funcionalidade de Views do padrão MVC, com a sua chamada em seu respectivo controller, que de forma dinâmica é capaz de montar seu diretório.

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -3,13 +3,33 @@
 
     class IndexController{
 
+        private $view;
+
+        //Função construtora responsável por tornar possível armazenar dados em um objeto vazio acessável pela view
+        public function __construct(){
+            $this->view = new  \stdClass();
+        }
+
         //Actions teste
         public function index(){
-            echo 'Action index';
+            $this->view->arrayTeste = array('DadoIndex1', 'DadoIndex2', 'DadosIndex3');
+            $this->render('index');
         }
 
         public function contato(){
-            echo 'Action contato';
+            $this->view->arrayTeste = array('DadosContato1', 'DadosContato2', 'DadosContato3');
+            $this->render('contato');
+        }
+
+        //Requisição da View pelo controller a partir do arquivo index.php
+        public function render($view){
+            //Encontrando dinamicamente o diretório da view solicitado pelo controller
+            $controller = get_class($this);
+            $controller = str_replace('App\\Controllers\\', '', $controller);
+            $controller = str_replace('Controller', '', $controller);
+            $controller = strtolower($controller);
+
+            require_once "../App/Views/" .$controller. "/" .$view. ".phtml";
         }
     }
 ?>
