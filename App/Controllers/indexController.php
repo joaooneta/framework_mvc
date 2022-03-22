7dfa356146902fe7cc705b1ff76eb029f17d8267
João Oneta <joaomatheusoneta01@gmail.com>
2022-03-22 15:40:06 -0300

    Desenvolvida a classe responsável pela conexão com o banco no diretório App, além da camada de Models do padrão MVC, com a criação de uma classe teste Produto interagindo com o banco. Também foram adicionadas as querys paraa criação do banco.

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -1,12 +1,25 @@
 <?php
     namespace App\Controllers;
     use MF\Controller\Action;
+    use App\Connection;
+    use App\Models\Produto;
 
     class IndexController extends Action{
 
         //Actions teste
         public function index(){
-            $this->view->arrayTeste = array('DadoIndex1', 'DadoIndex2', 'DadosIndex3');
+            //$this->view->arrayTeste = array('DadoIndex1', 'DadoIndex2', 'DadosIndex3');
+
+            //Recuperação da instância da conexão
+            $conn = Connection::getDb();
+
+            //Instância da classe teste Produto
+            $produto = new Produto($conn);
+
+            //Recuperando array de teste
+            $produtos = $produto->getProdutos();
+            $this->view->arrayTeste = $produtos;
+
             $this->render('index', 'layouthome');
         }
 
---
+++ App/Models/Produto.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Models;
+
+    class Produto{
+        protected $db;
+
+        public function __construct(\PDO $db){
+            $this->db = $db;
+        }
+
+        public function getProdutos(){
+            $query = 'SELECT id, descricao, preco FROM tb_produtos;';
+            return $this->db->query($query)->fetchAll();
+        }
+    }
+?>
